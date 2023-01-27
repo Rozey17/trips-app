bf4e9d32152ec60f3572f208f9f903cf4bab6ceb
Ferrad Makosso <36968544+Rozey17@users.noreply.github.com>
2023-01-27 17:03:29 +0100

    chore : modal

--- src/screens/Search.tsx
+++ src/screens/Search.tsx
@@ -5,14 +5,53 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { s } from "react-native-wind";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Entypo, FontAwesome5 } from "@expo/vector-icons";
+import { AntDesign, Entypo, FontAwesome5 } from "@expo/vector-icons";
 import Header from "../components/Header";
 import { Button } from "react-native-paper";
+import Modal from "react-native-modal";
 
 const Search = () => {
+  const [showInfoModal, setShowInfoModal] = useState(false);
+  function renderInfoModal() {
+    return (
+      <Modal
+        isVisible={showInfoModal}
+        onBackdropPress={() => setShowInfoModal(false)}
+        hideModalContentWhileAnimating={true}
+        backdropTransitionOutTiming={0}
+        style={{ margin: 0 }}
+      >
+        {/* Content */}
+        <View
+          // style={{
+          //   backgroundColor: "white",
+          //   position: "absolute",
+          //   width: "100%",
+          //   bottom: 0,
+          //   paddingHorizontal: 28,
+          //   paddingVertical: 40,
+
+          // }}
+          style={s`bg-white absolute p-5 h-full w-full bottom-0`}
+        >
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              right: 0,
+              top: 0,
+              padding: 16,
+            }}
+            onPress={() => setShowInfoModal(false)}
+          >
+            <AntDesign name="close" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    );
+  }
   return (
     <View style={s`flex-1`}>
       <Header />
@@ -45,13 +84,19 @@ const Search = () => {
             placeholder="To ..."
           />
 
-          <TouchableOpacity style={s`p-3 rounded mt-5 bg-sky-500`}>
+          <TouchableOpacity
+            style={s`p-3 rounded mt-5 bg-sky-500`}
+            onPress={() => setShowInfoModal(!showInfoModal)}
+          >
             <Text style={s` text-white font-medium text-center`}>Search</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
+      {renderInfoModal()}
     </View>
   );
 };
+
+
 
 export default Search;
